63745775d0472f00ed902fc0115a4544755dbfac
muhammad fachrurrozi <75963824+TheRaccoon-Black@users.noreply.github.com>
2022-06-03 20:15:03 +0700

    Create fungsi.cpp

---
+++ fungsi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+
+void luasjajargenjang(){
+	jajargenjang.luas= jajargenjang.pjg*jajargenjang.lbr;
+	printf("luas jajargenjang anda adalah : %d \n",jajargenjang.luas);
+}
+
+void kelilingjajargenjang(){
+	jajargenjang.kllg=2*(jajargenjang.pjg*jajargenjang.lbr);
+	printf("keliling jajargenjang anda adalah ; %d \n",jajargenjang.kllg);
+	
+}
